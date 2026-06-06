ebc8d4d18dd29ceb03ea56f098d56fb053cafca1
Erison Felipe <erisonfelipe1@outlook.com>
2026-06-06 08:43:14 -0300

    fix: force railway to get latest code

--- server/src/routes/boletoRoutes.js
+++ server/src/routes/boletoRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
-const { list, create } = require("../controllers/boletoController");
+const { list, create, remove } = require("../controllers/boletoController");
 
 router.use(authMiddleware);
 
