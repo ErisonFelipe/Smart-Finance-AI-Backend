bd80d8670a4c7f4e78a4af70c95865e5324e4a8e
Erison Felipe <erisonfelipe1@outlook.com>
2026-06-06 08:39:55 -0300

    fix: ensure remove is exported in debt and boleto controllers

--- server/src/routes/debtRoutes.js
+++ server/src/routes/debtRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
-const { list, create } = require("../controllers/debtController");
+const { list, create, remove } = require("../controllers/debtController");
 
 router.use(authMiddleware);
 
